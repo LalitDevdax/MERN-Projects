a77e14df5b7154c89568df8c9f9af6ff45143e36
Lalit Devda <95845301+LalitDevdax@users.noreply.github.com>
2024-11-23 09:48:22 -0800

    commit

--- MeowChat/backend/src/lib/utils.js
+++ MeowChat/backend/src/lib/utils.js
@@ -5,17 +5,3 @@
 
 import jwt from "jsonwebtoken";
 
-export const generateToken = async (userID, res) => {
-  const token = jwt.sign({ userID }, process.env.jwt_secret, {
-    expiresIn: "7d",
-  });
-
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, //millisec
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
-  });
-
-  return token;
-};
