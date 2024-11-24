602611c37dfbf2d443652ffeb4e04fb5248a7ff3
Lalit Devda <95845301+LalitDevdax@users.noreply.github.com>
2024-11-24 03:33:14 -0800

    update

--- MeowChat/backend/src/lib/utils.js
+++
@@ -1,7 +0,0 @@
-//generating token : to generate token we need environment varialbe
-//we need payload and res as parameter while generating token and take it as unique like userid
-//while creating token using sign method give payload as object and third parameter as object and in middle give secret key generated in env
-//then after creating token send it to cookies by res.cookies and provide first parameter as any name and other the token created and third as optional but it for more protection
-
-import jwt from "jsonwebtoken";
-
